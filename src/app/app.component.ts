e13e119133b4edb9bd6fa879be2f64988a1d2230
4stru <4.struong@kscholars.org>
2020-01-04 21:21:40 -0600

    json data not fetching

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,42 +9,42 @@ import { Satellite } from './satellite';
 export class AppComponent {
   title = 'orbit-report';
   sourceList: Satellite[];
-  constructor() {
-    this.sourceList = [
-       new Satellite("SiriusXM", "Communication", "2009-03-21", "LOW", true),
-       new Satellite("Cat Scanner", "Imaging", "2012-01-05", "LOW", true),
-       new Satellite("Weber Grill", "Space Debris", "1996-03-25", "HIGH", false),
-       new Satellite("GPS 938", "Positioning", "2001-11-01", "HIGH", true),
-       new Satellite("ISS", "Space Station", "1998-11-20", "LOW", true),
-    ];
- }
+//   constructor() {
+//     this.sourceList = [
+//        new Satellite("SiriusXM", "Communication", "2009-03-21", "LOW", true),
+//        new Satellite("Cat Scanner", "Imaging", "2012-01-05", "LOW", true),
+//        new Satellite("Weber Grill", "Space Debris", "1996-03-25", "HIGH", false),
+//        new Satellite("GPS 938", "Positioning", "2001-11-01", "HIGH", true),
+//        new Satellite("ISS", "Space Station", "1998-11-20", "LOW", true),
+//     ];
+//  }
 
 // loop for showing json data not working for some reason
-//  constructor() {
-//   this.sourceList = [];
-//   let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+ constructor() {
+  this.sourceList = [];
+  let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
 
-//   window.fetch(satellitesUrl).then(function(response) {
-//      response.json().then(function(data) {
+  window.fetch(satellitesUrl).then(function(response) {
+     response.json().then(function(data) {
 
-//         let fetchedSatellites = data.satellites;
+        let fetchedSatellites = data.satellites;
 
-//         for (let i = 0; i < fetchedSatellites; i++) {
-//             let satellite = new Satellite(
-//               fetchedSatellites[i].name,
-//               fetchedSatellites[i].type,
-//               fetchedSatellites[i].launchDate,
-//               fetchedSatellites[i].orbitType,
-//               fetchedSatellites[i].operational
-//             );
+        for (let i = 0; i < fetchedSatellites; i++) {
+            let satellite = new Satellite(
+              fetchedSatellites[i].name,
+              fetchedSatellites[i].type,
+              fetchedSatellites[i].launchDate,
+              fetchedSatellites[i].orbitType,
+              fetchedSatellites[i].operational
+            );
 
-//             this.sourceList.push(satellite);
-//           }
+            this.sourceList.push(satellite);
+          }
 
-//      }.bind(this));
-//   }.bind(this));
+     }.bind(this));
+  }.bind(this));
 
-// }
+}
 
 
 
