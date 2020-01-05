abfe5c59b256275fcb08bc6d49a5ea31627a7858
4stru <4.struong@kscholars.org>
2020-01-04 22:13:36 -0600

    working search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,19 +9,12 @@ import { Satellite } from './satellite';
 export class AppComponent {
   title = 'orbit-report';
   sourceList: Satellite[];
-  //   constructor() {
-  //     this.sourceList = [
-  //        new Satellite("SiriusXM", "Communication", "2009-03-21", "LOW", true),
-  //        new Satellite("Cat Scanner", "Imaging", "2012-01-05", "LOW", true),
-  //        new Satellite("Weber Grill", "Space Debris", "1996-03-25", "HIGH", false),
-  //        new Satellite("GPS 938", "Positioning", "2001-11-01", "HIGH", true),
-  //        new Satellite("ISS", "Space Station", "1998-11-20", "LOW", true),
-  //     ];
-  //  }
+  displayList: Satellite[];
 
-  // loop for showing json data not working for some reason
   constructor() {
     this.sourceList = [];
+    this.displayList = [];
+
     let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
 
     window.fetch(satellitesUrl).then(function (response) {
@@ -45,6 +38,18 @@ export class AppComponent {
 
   }
 
-
+  search(searchTerm: string): void {
+    let matchingSatellites: Satellite[] = [];
+    searchTerm = searchTerm.toLowerCase();
+    for(let i=0; i < this.sourceList.length; i++) {
+       let name = this.sourceList[i].name.toLowerCase();
+       if (name.indexOf(searchTerm) >= 0) {
+          matchingSatellites.push(this.sourceList[i]);
+       }
+    }
+    // assign this.displayList to be the the array of matching satellites
+    // this will cause Angular to re-make the table, but now only containing matches
+    this.displayList = matchingSatellites;
+ }
 
 }
